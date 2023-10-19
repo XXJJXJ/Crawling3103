8514d7d1f85388ea487a08f262265af8fec4d042
Ye Xinjian <yxj@Yes-MacBook-Air.singtel.com>
2023-10-19 23:33:48 +0800

    Add timeout to requests

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 class SafeList:
+    '''This SafeList class is protected by a mutex, it stores a queue of URLs to be visited'''
     def __init__(self):
         self.list = []
         self.mutex = threading.Lock()
@@ -22,6 +23,7 @@ class SafeList:
         return len(self.list) == 0
         
 class SafeMap:
+    '''This SafeMap class has protected access by a mutex, it stores all visited domains'''
     def __init__(self):
         self.map = {}
         self.mutex = threading.Lock()
@@ -35,6 +37,7 @@ class SafeMap:
         return item in self.map
 
 class Site:
+    '''This is a class wrapper for a website and the information required by the assignment'''
     def __init__(self, url, ip, geolocation, resp_time):
         self.url = url
         self.ip = ip
@@ -42,6 +45,7 @@ class Site:
         self.response_time = resp_time
 
 class SafeWriter:
+    '''This is a SafeWriter that utilizes a mutex to lock access to the file "scraped.txt"'''
     def __init__(self):
         self.mutex = threading.Lock()
 
@@ -51,14 +55,22 @@ class SafeWriter:
                 f.write("{},{},{},{}\n".format(s.response_time, s.geolocation, s.ip, s.url))
 
 def get_location(ip):
+    '''This function utilizes the ip-api API to find geolocation of ips. 
+       Might have chance of denial of service due to too high request rate'''
     if ip == "":
         return "Country Not Found"
     attempt = 0
     country = None
-    # try 5 times
-    while country is None or attempt < 5:
-        response = requests.get(f'https://ipapi.co/{ip}/json/').json()
-        country = response.get("country_name")
+    # try 3 times
+    while country is None and attempt < 3:
+        if attempt > 0:
+            time.sleep(3)
+        try:
+            #response = requests.get(f'https://ipapi.co/{ip}/json/', timeout=5).json()
+            response = requests.get(f'http://ip-api.com/json/{ip}', timeout=5).json()
+            country = response.get("country_name")
+        except:
+            print("Either timeout or faulty response")
         attempt += 1
 
     if country:
@@ -67,18 +79,21 @@ def get_location(ip):
         return "Country Not Found"
 
 def get_ip(url: str):
+    '''This function returns the ip address of the url'''
     url = url.replace("https://", "", 1)
     url = url.replace("http://", "", 1)
     ip = socket.gethostbyname(url)
     return ip
 
 def scrapper(ls: SafeList, m: SafeMap, w: SafeWriter):
+    '''This is the main scrapper logic to be run on threads.
+       They share the same SafeList, SafeMap and SafeWriter'''
     while not ls.isEmpty():
         url = ls.pop()
         start = time.time()
         try:
             print(url)
-            resp = requests.get(url)
+            resp = requests.get(url, timeout=5)
             time_taken = str(round((time.time() - start), 2))
             ip = get_ip(url)
             location = get_location(ip)
@@ -113,8 +128,6 @@ if __name__ == '__main__':
         ls.batch_insert(urls)
         m.batch_insert(urls)
 
-    # print("Starting URLs:\n")
-    # print(urls)
     # initialize threads and start them
     threadList = []
     for i in range(3):
